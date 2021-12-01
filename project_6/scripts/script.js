cdb9d21404d23d9b270614454a847c8eb1be38f6
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-01 06:19:57 -0700

    Changed heading color

--- project_6/scripts/script.js
+++ project_6/scripts/script.js
@@ -9,5 +9,13 @@ var color = 'purple';
 document.querySelector('.left').style.backgroundColor = color;
 document.querySelector('.left .color-value').innerHTML = color;
 
+color = 'skyblue';
+
 document.querySelector('.right').style.backgroundColor = color;
 document.querySelector('.right .color-value').innerHTML = color;
+
+function headingColor() {
+  color = 'blue';
+  document.querySelector('.title').style.color = color;
+}
+headingColor();
