8e90c4a1eb32f343b80928f5c7a9c46063f5d393
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-01 05:38:08 -0700

    removed folders from branch

--- project_5/scripts/script.js
+++
@@ -1,6 +0,0 @@
-/**
- * The var statement
- * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
- */
-
-// var statements are mutable: You can change its value at any time and change it
