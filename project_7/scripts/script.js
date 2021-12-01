9b9089059488ae8a1a427f68b492a136d8a9c960
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-01 14:42:25 -0700

    intitial commit for project_7

---
+++ project_7/scripts/script.js
@@ -0,0 +1,13 @@
+/**
+ *  Scope and the var statement
+ *
+ *
+ */
+
+var color = 'purple';
+
+document.querySelector('.left').style.backgroundColor = color;
+document.querySelector('.left .color-value').innerHTML = color;
+
+document.querySelector('.right').style.backgroundColor = color;
+document.querySelector('.right .color-value').innerHTML = color;
