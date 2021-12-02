c5a389ea4701108780b3027a7d52f1a7f480e848
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-01 20:24:05 -0700

    initial commit

---
+++ project_11/scripts/script.js
@@ -0,0 +1,27 @@
+/**
+ * Working with function
+ * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
+ */
+
+const greenPack = {
+    name: 'Frog Pack',
+    color: 'green',
+    volume: 8,
+    pocketNum: 3
+}
+
+const addPack = function(currentPack) {
+    const newArticle = document.querySelector('article');
+    newArticle.innerHTML(`
+    <h1>${currentPack.name}</h1>
+    <ul>
+    <li>Volume: ${currentPack.volume} </li>
+    <li>Color: ${currentPack.Color} </li>
+    <li>Number of pockets: ${currentPack.pockNum} </li>
+    </ul>
+    `);
+    return newArticle
+}
+
+const main = document.querySelector('main');
+main.append(addPack(greenPack));
