7229bc608c373848db8e8021559237fec8caf623
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-02 05:18:16 -0700

    end of example

--- project_14/scripts/script.js
+++ project_14/scripts/script.js
@@ -2,3 +2,25 @@
  * Passing data to functions with paramaters via arguments
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
  */
+
+const tipCalculator = function(sum, percentage, currency, prefix) {
+    let tip = sum * (percentage / 100)
+    let total = sum + tip
+    if (prefix) {
+        console.log(`
+        Sum before tip: ${currency}${sum.toFixed(2)}
+        Tip percentage: ${percentage}%
+        Tip: ${currency}${tip.toFixed(2)}
+        Total: ${currency}${total.toFixed()}
+        `)
+    } else {
+        console.log(`
+        Sum before tip: ${sum.toFixed(2)}${currency}
+        Tip percentage: ${percentage}%
+        Tip: ${tip.toFixed(2)}${currency}
+        Total: ${total.toFixed()}${currency}
+`)
+    }
+
+}
+tipCalculator(34.50, 10, '$', true)
