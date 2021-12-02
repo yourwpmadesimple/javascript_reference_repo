92ffba95dbc17183579ac786fc5745a44bb4fc92
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-02 08:27:43 -0700

    project_18 initial commit

---
+++ project_18/scripts/script.js
@@ -0,0 +1,7 @@
+/**
+ * Create a new element and append it to the <main> element.
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/append
+ */
+
+import Backpack from "./Backpack.js";
