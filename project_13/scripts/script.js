ce9007ca1f7c8c27d2839e289cae7bb8eb1fc1b7
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-02 04:44:06 -0700

    initial commit

--- project_13/scripts/script.js
+++ project_13/scripts/script.js
@@ -1,20 +1,4 @@
 /**
- * functions with this keyword
+ * Passing data to functions with paramaters via arguments
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
  */
-window.volume = 20
-
-const greenPack = {
-    name: 'Frog Pack',
-    color: 'green',
-    volume: 8,
-    pocketNum: 3,
-    newVolume: function(volume) {
-        console.log('this.volume is in the method: ', this.volume)
-        this.volume = volume
-        console.log('this.volume after update: ', this.volume)
-        return this.volume
-    }
-};
-
-console.log(greenPack.newVolume(5))
