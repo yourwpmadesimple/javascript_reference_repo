2948cadc395b753c83db0986b5bde8d94ddb4089
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-01 20:01:10 -0700

    end of exercise

--- project_10/scripts/script.js
+++ project_10/scripts/script.js
@@ -14,7 +14,17 @@ const doMoreMath = function(a, b) {
     let c = a * b
     return c
 }
-console.log(doSomeMath)
 
-console.log(`Do some math: ${doSomeMath(5, 6)}`)
-console.log(`Do more math ${doMoreMath(2, 8)}`)
+console.log(doSomeMath)
+console.log(`Do some math: ${doSomeMath(5, 6)}`) // Expected outcome {11}
+
+console.log(doMoreMath)
+console.log(`Do more math ${doMoreMath(2, 8)}`) // Expected outcome {16}
+
+// Immediately Invoked Function Expression (IIFE)
+(function() {
+    let a = 4;
+    let b = 6;
+    let c = doSomeMath(a, b);
+    console.log(`The sum of a and b is: ${c}`);
+})(); // Invoked functions do not seem to return console.log data
