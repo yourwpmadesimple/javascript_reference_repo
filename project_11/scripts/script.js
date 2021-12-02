9a98290dd8c0e36e8975d33e9ac28fab2f16c9cb
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-01 21:56:05 -0700

    end of standard funtions demo

--- project_11/scripts/script.js
+++ project_11/scripts/script.js
@@ -1,5 +1,5 @@
 /**
- * Working with function
+ * Standard functions
  * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
  */
 
@@ -12,16 +12,19 @@ const greenPack = {
 
 const addPack = function(currentPack) {
     const newArticle = document.querySelector('article');
-    newArticle.innerHTML(`
+    newArticle.innerHTML = `
     <h1>${currentPack.name}</h1>
     <ul>
     <li>Volume: ${currentPack.volume} </li>
     <li>Color: ${currentPack.Color} </li>
     <li>Number of pockets: ${currentPack.pockNum} </li>
     </ul>
-    `);
+    `;
     return newArticle
 }
+const theArticle = addPack(greenPack);
+console.log(theArticle)
+
 
 const main = document.querySelector('main');
-main.append(addPack(greenPack));
+main.append(theArticle);
