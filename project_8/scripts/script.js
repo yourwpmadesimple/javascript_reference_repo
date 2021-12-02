befd416414e3fe57703757002dd25e86a3ded7e8
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-01 17:22:40 -0700

    array vaiants

--- project_8/scripts/script.js
+++ project_8/scripts/script.js
@@ -1,26 +1,15 @@
 /**
- *  Local scope 'let'
+ *  Arrrays: assignments, changes
  *
  *
  */
 
-var color = 'purple';
+const items = ['apple', 'pen', 3, true, 'mittens']
 
-document.querySelector('.left').style.backgroundColor = color;
-document.querySelector('.left .color-value').innerHTML = color;
-
-color = 'skyblue';
-
-function headingTitle() {
-    let titleColor = 'blue';
-    document.querySelector('.title').style.color = titleColor;
-    console.log(`inside ${titleColor}`);
-}
-headingTitle();
-
-// console.log('side', titleColor) // This will throw an error of undefined titleColor and the code stops excuting and resolves to the css definition for the code below.
-
-document.querySelector('.right').style.backgroundColor = color;
-document.querySelector('.right .color-value').innerHTML = color;
-
-console.log('side', titleColor); // This will throw and error Uncaught ReferenceError: titleColor is not defined. However, all the Javascript above will execute just fine.
+items.push('Wayne')
+console.log(items);
+items[2] = 'Chicago';
+let newItem = items;
+console.log(items);
+let anotherNewItem = items.pop()
+console.log(`"${newItem}" was added to the 3rd posistion in the array and ${anotherNewItem} was removed from the end`);
