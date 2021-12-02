2e89db24dd3974f1da709dffc2ff4665ac431c25
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-02 04:41:04 -0700

    ammended file to showcase the 'this' keyword

--- project_13/scripts/script.js
+++ project_13/scripts/script.js
@@ -2,6 +2,7 @@
  * functions with this keyword
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
  */
+window.volume = 20
 
 const greenPack = {
     name: 'Frog Pack',
@@ -9,26 +10,11 @@ const greenPack = {
     volume: 8,
     pocketNum: 3,
     newVolume: function(volume) {
-        this.volume = volume
         console.log('this.volume is in the method: ', this.volume)
+        this.volume = volume
+        console.log('this.volume after update: ', this.volume)
+        return this.volume
     }
-}
+};
 
-const addPack = (currentPack) => {
-    const newArticle = document.querySelector('article');
-    newArticle.innerHTML = `
-    <h1>${currentPack.name}</h1>
-    <ul>
-    <li>Volume: ${currentPack.volume} </li>
-    <li>Color: ${currentPack.Color} </li>
-    <li>Number of pockets: ${currentPack.pockNum} </li>
-    </ul>
-    `;
-    return newArticle
-}
-const theArticle = addPack(greenPack);
-console.log(theArticle)
-
-
-const main = document.querySelector('main');
-main.append(theArticle);
+console.log(greenPack.newVolume(5))
